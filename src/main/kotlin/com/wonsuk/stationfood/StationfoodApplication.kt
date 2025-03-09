1714ffafd55e9e1e14ed364b00b82da4b5bc546f
tjdjdjrdl123 <lee-won-suk@github.io>
2025-03-09 12:10:30 +0900

    feat: 테이블 설계 추가 초기세팅

---
+++ src/main/kotlin/com/wonsuk/stationfood/StationfoodApplication.kt
@@ -0,0 +1,11 @@
+package com.wonsuk.stationfood
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StationfoodApplication
+
+fun main(args: Array<String>) {
+    runApplication<StationfoodApplication>(*args)
+}
